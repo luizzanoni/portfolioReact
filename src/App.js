717a0bf10a7ce83400a5046f7dcebd7691e45fc8
Luiz Gustavo Zanoni <luizzanoni47@gmail.com>
2023-01-02 20:26:24 -0300

    adicionando contatos

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { Header } from './components/header/Header';
 import { Home } from './components/home/Home';
 import { About } from './components/about/About';
 import Skills from './components/skills/Skills';
+import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
+// import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
 
 export const App = () => {
   return (
@@ -14,6 +18,10 @@ export const App = () => {
       <Home />
       <About />
       <Skills />
+      <Services />
+      <Qualification />
+      {/* <Testimonials /> */}
+      <Contact />
     </main>
     </>
   )

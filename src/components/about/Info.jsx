1f04826cf672c8c273c14205cfc6a01eb688017c
Luiz Gustavo Zanoni <luizzanoni47@gmail.com>
2023-01-02 19:44:03 -0300

    adicionando about

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <i className="bx bx-award"></i>
+
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">8 years Working</span>
+      </div>
+
+      <div className="about__box">
+      <i className="bx bx-briefcase-alt"></i>
+
+        <h3 className="about__title"></h3>
+        <span className="about__subtitle"> 48 + Projects</span>
+      </div>
+
+      <div className="about__box">
+      <i className="bx bx-support about__icon"></i>
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">Online 24/7</span>
+      </div>
+    </div>
+  )
+}
+
+export default Info

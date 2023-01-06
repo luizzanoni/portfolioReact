9cce4172e1edc906ab6b9ad620c47cbf1d8ca2e1
luiz.zanoni <luiz.zanoni@CLOVIS>
2023-01-06 15:30:34 -0300

    remoção do gitlab

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export const Social = () => {
   return (
     <div className="home__social">
+      
       <a href="https://www.instagram.com/luiz.gzanoni/" className="home__social-icon" target="noreferrer">
         <i class="uil uil-instagram"></i>
       </a>
@@ -17,10 +18,6 @@ export const Social = () => {
 
       <a href="https://twitter.com/luiz_gzanoni" className="home__social-icon" target="noreferrer">
         <i class="uil uil-twitter-alt"></i>
-      </a>
-
-      <a href="https://gitlab.com/luizzanoni1" className="home__social-icon" target="noreferrer">
-        <i class="uil uil-gitlab"></i>
       </a>
       
     </div>

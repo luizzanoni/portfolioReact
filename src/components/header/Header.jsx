92a41f30a63112d6171ff0ef8896ed1b15544d3d
luiz.zanoni <luiz.zanoni@CLOVIS>
2023-01-06 10:04:34 -0300

    adc button dark theme

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,36 @@ export const Header = () => {
     if (this.scrollY >= 80) header.classList.add("scroll-header");
     else header.classList.remove("scroll-header");
   });
-  
+
+  // const themeButton = document.getElementById('theme-button')
+  // const darkTheme = 'dark-theme'
+  // const iconTheme = 'uil-sun'
+
+  // // Previously selected topic (if user selected)
+  // const selectedTheme = localStorage.getItem('selected-theme')
+  // const selectedIcon = localStorage.getItem('selected-icon')
+
+  // // We obtain the current theme that the interface has by validating the dark-theme class
+  // const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+  // const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+  // // We validate if the user previously chose a topic
+  // if (selectedTheme) {
+  //   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  //   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  //   themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+  // }
+
+  // // Activate / deactivate the theme manually with the button
+  // themeButton.addEventListener('click', () => {
+  //   // Add or remove the dark / icon theme
+  //   document.body.classList.toggle(darkTheme)
+  //   themeButton.classList.toggle(iconTheme)
+  //   // We save the theme and the current icon that the user chose
+  //   localStorage.setItem('selected-theme', getCurrentTheme())
+  //   localStorage.setItem('selected-icon', getCurrentIcon())
+  // })
+
   /* === toggle menu === */
   const [Toggle, showMenu] = useState(false);
 
@@ -52,6 +81,16 @@ export const Header = () => {
           </ul>
 
           <i class="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
+        </div>
+        
+        <div className="nav__btns">
+          {/* Theme change button */}
+           <i className="uil uil-moon change-theme" onClick={() => showMenu(!Toggle)}></i>
+
+          <div className="nav__toggle" id="nav__toggle">
+            <i className="uil uil-apps"></i>
+          </div> 
+
         </div>
 
         <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>

cc233246d87ba914db7b999cd0bc461b2688b57c
luiz.zanoni <luiz.zanoni@CLOVIS>
2023-01-06 11:56:57 -0300

    atualização de exp m8

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -30,32 +30,12 @@ const Services = () => {
               <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
 
               <h3 className="services__modal-title">Developer Full-Stack</h3>
-              <p className="services__modal-description">Service with more than 3 years of experience.Providing quality work to clients and companies.</p>
+              <p className="services__modal-description">HTML, CSS, dotNET, JavaScript, React, API's, ASP.NET Core, MVC, jQuery</p>
 
               <ul className="services__modal-services grid">
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">I develop the user interface.</p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Web page development.</p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">I create ux element interactions.</p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">I position your company brand.</p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Design and mockups of products for companies.</p>
+                  <p className="services__modal-info">Setembro 2022 - Presente</p>
                 </li>
               </ul>
             </div>

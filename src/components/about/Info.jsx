88e9fcb778da8901b452df67792531f50c8a27af
luiz.zanoni <luiz.zanoni@CLOVIS>
2023-01-16 15:28:03 -0300

    att exp

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -5,7 +5,7 @@ const Info = () => {
     <div className="about__info grid">
       <div className="about__box">
         <i className="bx bx-award about__icon"></i>
-        <h3 className="about__title">Experiência</h3>
+        <h3 className="about__title">Experiência Developer</h3>
         <span className="about__subtitle">+1 ano</span>
       </div>
 
